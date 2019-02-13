ec93e58abae89ac9a5930a5a3b8f25499a314eed
Sebass van Boxel <svboxel@gmail.com>
2019-02-13 16:30:07 +0100

    Test new payload

--- .github/proxy_request/entrypoint.sh
+++ .github/proxy_request/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-set -e
 
-declare -a endpoints=("https://api.appcenter.ms/v0.1/public/apps/8c335981-875d-4037-8618-17de0626dfd2/hooks" "https://smee.io/vHU5WwXJYKEG6ZKR" "https://aaaaa3.nl")
+declare -a endpoints=(
+   "https://smee.io/vHU5WwXJYKEG6ZKR" 
+   "https://api.appcenter.ms/v0.1/public/apps/8c335981-875d-4037-8618-17de0626dfd2/hooks"
+   )
 
 ## now loop through the above array
 for i in "${endpoints[@]}"
 do
    echo "Proxying payload to $i"
-   curl --header "Content-Type: application/json" -d "data=@${GITHUB_EVENT_PATH}"  $i
+   curl --header "Content-Type: application/json" --data-binary "@${GITHUB_EVENT_PATH}" $i
 done
