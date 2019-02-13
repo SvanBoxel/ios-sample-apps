997165f3594c13d45889418441bbe6df31ba8852
Sebass van Boxel <svboxel@gmail.com>
2019-02-13 16:01:28 +0100

    Updated loop

--- .github/proxy_request/entrypoint.sh
+++ .github/proxy_request/entrypoint.sh
@@ -1,8 +1,10 @@
+#!/bin/sh -l
+
 declare -a endpoints=("https://aaaaa1.nl" "https://aaaaa2.nl" "https://aaaaa3.nl")
 
 ## now loop through the above array
-for i in "${arr[@]}"
+for i in "${endpoints[@]}"
 do
    echo "Proxying payload to $i"
-   curl --header "Content-Type: application/json" -d "{\"value\":\"node JS\"}" $1
+   curl --header "Content-Type: application/json" -d "{\"value\":\"node JS\"}" $i
 done
