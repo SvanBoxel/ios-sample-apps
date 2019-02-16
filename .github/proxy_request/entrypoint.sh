7b10caaac1b4a401977b2a1f2b4c7fe3fa4c056f
Sebass van Boxel <svboxel@gmail.com>
2019-02-16 21:25:57 +0100

    Update entrypoint.sh

--- .github/proxy_request/entrypoint.sh
+++ .github/proxy_request/entrypoint.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-declare -a endpoints=(
-   "https://smee.io/vHU5WwXJYKEG6ZKR"
-   "https://api.appcenter.ms/v0.1/public/apps/8c335981-875d-4037-8618-17de0626dfd2/hooks"
-   )
+#!/bin/sh -l
 
-## now loop through the above array
-for i in "${endpoints[@]}"
-do
-   echo "Proxying payload to $i"
-   curl --header "Content-Type: application/json" --header "X-GitHub-Delivery: ${RANDOM}" --data-binary "@${GITHUB_EVENT_PATH}" $i
-done
+npm install
+node index.js
